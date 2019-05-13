05a0e5e117c65c61a448b9c4fef90e62d60ecedd
Aaron P <aaron4uk@gmail.com>
2019-05-12 20:42:37 -0500

    implimented first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Welcome from './components/pages/Welcome'
+
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<Welcome user={{id: 5, permission: true}} />)
 }
 
-export default App;
+export default App
--- src/components/pages/Welcome.tsx
+++ src/components/pages/Welcome.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
 
+interface objUser {
+  id: number,
+  permission: boolean,
+}
+
 interface props {
- name: string,
+ name?: string,
+ user: objUser,
 }
 const Welcome: React.SFC<props> = ({
   name,
+  user,
 }) => {
- return <h1>Hello, {name}</h1>;
+ return <h1>Welcome back, {name || `#00${user.id}`}.</h1>
 }
+
+export default Welcome
