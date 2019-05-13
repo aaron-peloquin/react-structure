b91377e6b3bab104f7a3e203891266528bcebacc
Aaron P <aaron4uk@gmail.com>
2019-05-13 18:55:23 -0500

    adding comments

--- src/components/pages/Welcome.tsx
+++ src/components/pages/Welcome.tsx
@@ -5,6 +5,20 @@ interface objUser {
   permission: boolean,
 }
 
+/**
+ * This style of types with `interface` can be exported and shared between components, but I think that is a bad pattern.
+ * However, not exporting it would most likely be easier to read
+ */
+// interface props{
+//   name?: string,
+//   message?: string,
+//   user: {
+//     id: number,
+//     permission: boolean,
+//     displayName?: string,
+//   },
+//  }
+
 // `React.SFC` is a "Stateless Functional Component" interface
 const Welcome: React.SFC<{ // This object defines our prop types
   name?: string,
@@ -13,15 +27,21 @@ const Welcome: React.SFC<{ // This object defines our prop types
     id: number,
     permission: boolean,
     displayName?: string,
+    invisibleProps?: {
+      doesNotCauseReRender: boolean,
+    }
   },
  }> = ({ // This object is deconstructed into our variables we can use within the component
   name,
   message = 'Have a nice day',
   user: {
     id,
-    ...otherUserKeys // See below where we are accessing `displayName` and `permission`
+    ...otherUserKeys
   },
 }) => {
+  // Note for `otherUserKeys`
+  // This will catch all remaining keys that were not already destructured and throw them into this object.
+  // Function components will only shallow compare when deciding if it should re-render, so changing `doesNotCauseReRender` does not trigger a new render of this component.
  return <React.Fragment>
    <h1>Welcome back, {name || `User #${id.toString().padStart(2, '0')}`}..</h1>
    <p>
