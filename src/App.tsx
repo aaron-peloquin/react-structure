116ffcdd9852cd5922d7a6c7e0c37d07dad3426d
Aaron P <aaron4uk@gmail.com>
2019-05-14 18:34:07 -0500

    updating props and comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,17 @@ import './App.css';
 import Welcome from './components/pages/Welcome'
 
 // Bad pattern of having let outside of component and mutating inside. Props are not passed down to trigger a re-render
-let sneakyData = 5
 const user = {
   id: 5,
   permission: true,
-  invisibleProp: sneakyData
+  invisibleProp: 4444,
 }
 
 const App: React.FC = () => {
-  const [badNumber, setBadNumber] = React.useState(999)
-  sneakyData = badNumber
-
-  const updateNumber = () => {
-    const number = Math.ceil(Math.random()*500)
-    setBadNumber(number)
-  }
-
-  console.log('Rendering <App /> with', badNumber)
-
-  return (<React.Fragment>
-    <button onClick={updateNumber}>New Number</button>
-    <p>Number is: {badNumber}</p>
-    <Welcome
-      name="Aaron"
-      user={user}
-      
-    />
-  </React.Fragment>)
+  return <Welcome
+    name="Aaron"
+    user={user}
+  />
 }
 
 export default React.memo(App)
--- src/components/pages/Welcome.tsx
+++ src/components/pages/Welcome.tsx
@@ -21,13 +21,10 @@ const Welcome: React.SFC<{ // This object is the interface that defines our prop
   message = 'Have a nice day',
   user: { // by destructuring `user` here, we no longer have access to `user` in our component
     id,
-    invisibleProp,
-    ...otherUserKeys // The `...{variableName}` syntax is the `rest pattern`. It will catch all keys that were not deconstructed from the user object (permission & displayName), and assign them into `otherUserKeys`
+    ...otherUserKeys // This `...{variableName}` syntax is the `rest pattern`. It will catch all keys that were not deconstructed from the user object (permission & displayName), and assign them into `otherUserKeys`
   },
-  // Note: The prop `invisibleProp` is not being deconstructed or caught by a a restructuring (...), so it is essentially unavaible and ignored, even though its required by the interface - don't do this.
-  // Additionally, the `invisibleProp` data is being passed in as a reference to a let variable, which is being mutated. Since the reference is not updating, this component does not re-render
 }) => {
-  console.log('Rendering <Welcome />', invisibleProp)
+  console.log('Rendering <Welcome />', otherUserKeys.invisibleProp)
   return <React.Fragment>
     <h1>Welcome back, {name || `User #${id.toString().padStart(2, '0')}`}..</h1>
     <p>
@@ -40,17 +37,3 @@ const Welcome: React.SFC<{ // This object is the interface that defines our prop
 
 // Because this is a SFC, we can feel safe to memoize the results to skip some future re-render
 export default React.memo(Welcome)
-
-/**
- * This style of types with `interface` can be exported and shared between components, but I think that is a bad pattern.
- * However, not exporting it would most likely be easier to read
- */
-// interface props{
-//   name?: string,
-//   message?: string,
-//   user: {
-//     id: number,
-//     permission: boolean,
-//     displayName?: string,
-//   },
-//  }
